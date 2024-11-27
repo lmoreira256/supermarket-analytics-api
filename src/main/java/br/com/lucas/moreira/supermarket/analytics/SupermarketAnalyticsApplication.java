a7626405251c3b73502945878d199dc1f62c46a0
Lucas Moreira <lucamoreira256@gmail.com>
2024-11-27 16:14:18 -0300

    Criado projeto

---
+++ src/main/java/br/com/lucas/moreira/supermarket/analytics/SupermarketAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucas.moreira.supermarket.analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketAnalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lucas/moreira/supermarket/analytics/SupermarketAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucas.moreira.supermarket.analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
